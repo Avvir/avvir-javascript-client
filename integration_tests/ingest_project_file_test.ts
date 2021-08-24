52432f693b6067acdbe4e19461f03178c1142651
Trevor <trevor@avvir.io>
2021-08-24 16:19:04 -0400

    WIP: login working

--- integration_tests/ingest_project_file_test.ts
+++ integration_tests/ingest_project_file_test.ts
@@ -1,6 +1,5 @@
 import AuthApi from "../source/api/auth_api";
 import {describe} from "mocha";
-// import Avvir from "../avvir";
 import AvvirApi from "../source/avvir_api";
 import ApiCloudFile from "../source/models/api/api_cloud_file";
 import {OTHER} from "../source/models/enums/purpose_type";
@@ -8,74 +7,62 @@ import Pipelines from "../source/models/enums/pipeline_types";
 import ApiPipeline, {ApiPipelineArgument} from "../source/models/api/api_pipeline";
 import {expect} from "chai";
 import RunningProcessStatus from "../source/models/enums/running_process_status";
+import {User} from "../source/utilities/get_authorization_headers";
 
 describe("Ingest project files test", () => {
-    let projectId, user, email, password, checkPipeline
-    beforeEach(() => {
-        email = process.env.AVVIR_SANDBOX_EMAIL
-        password = process.env.AVVIR_SANDBOX_PASSWORD
-        projectId = process.env.AVVIR_SANDBOX_PROJECT
+  let projectId: string, user: User, email: string, password: string, checkPipeline
+  beforeEach(() => {
+    email = process.env.AVVIR_SANDBOX_EMAIL
+    password = process.env.AVVIR_SANDBOX_PASSWORD
+    projectId = process.env.AVVIR_SANDBOX_PROJECT_ID
 
-        checkPipeline = (pipelineResponse, index = 0) => {
-            return new Promise((resolve, reject)=> {
-                AvvirApi.other.checkPipelineStatus({projectId}, pipelineResponse.id, user)
-                .then((response)=>{
-                    if(index > 10) {
-                        reject("Too Many Calls: Check endpoint to make sure the implementation isn't flawed.")
-                    }
-                    else if(response.status !== RunningProcessStatus.COMPLETED) {
-                        resolve(checkPipeline(response.id, ++index))
-                    }else {
-                        resolve(response);
-                    }
-                })
+    checkPipeline = (pipelineResponse, index = 0) => {
+      return new Promise((resolve, reject) => {
+        AvvirApi.other.checkPipelineStatus({projectId}, pipelineResponse.id, user)
+            .then((response) => {
+              if (index > 10) {
+                reject("Too Many Calls: Check endpoint to make sure the implementation isn't flawed.")
+              } else if (response.status !== RunningProcessStatus.COMPLETED) {
+                resolve(checkPipeline(response.id, ++index))
+              } else {
+                resolve(response);
+              }
             })
-        }
-    })
+      })
+    }
+  })
 
-    it("Copies the file into Avvir's cloud storage", () => {
-        let apiCloudFile: ApiCloudFile = new ApiCloudFile({url: "TODO: get a file", purposeType: OTHER})
+  it("Copies the file into Avvir's cloud storage", (done) => {
+    let apiCloudFile: ApiCloudFile = new ApiCloudFile({url: 'home/trevor/Desktop/foo.txt', purposeType: OTHER})
 
-        let pipelineResponse: ApiPipeline;
+    let pipelineResponse: ApiPipeline;
 
-        AuthApi.login("avvir-client-integration-user@example.com", "password")
-            .catch((err)=>{
-                console.log(err);
-            })
-        .then((user)=>{
-            console.log("logged in", user);
-            AvvirApi.files.createProjectFile({projectId}, apiCloudFile, user)
-            .then((apiCloudFile) => {
+    AuthApi.login(email, password)
+        .then((user) => {
+          console.log("logged in", user);
+          AvvirApi.files.createProjectFile({projectId}, apiCloudFile, user)
+              .then((apiCloudFile) => {
                 let pipeline: ApiPipelineArgument = new ApiPipeline({
-                    name: Pipelines.INGEST_PROJECT_FILE,
-                    firebaseProjectId: projectId,
-                    options: {url: apiCloudFile.url}
+                  name: Pipelines.INGEST_PROJECT_FILE,
+                  firebaseProjectId: projectId,
+                  options: {url: apiCloudFile.url}
                 })
-                AvvirApi.pipelines.triggerPipeline(pipeline, user).then((pipelineResponse)=>{
-                    expect(pipelineResponse.status).to.eq(RunningProcessStatus.RUNNING)
+                AvvirApi.pipelines.triggerPipeline(pipeline, user).then((pipelineResponse) => {
+                  expect(pipelineResponse.status).to.eq(RunningProcessStatus.RUNNING)
                 });
 
                 checkPipeline(pipelineResponse)
-                    .catch((err)=>{
-                        console.log(err)
-                    })
-                    .then((response)=>{
-                        console.log(response);
+                    .then((response) => {
+                      console.log(response);
                     });
-                // do {
-                //     AvvirApi.other.checkPipelineStatus({projectId}, pipelineResponse.id, user)
-                //     setTimeout( () =>{
-                //         console.log("Waited 10 seconds, checking pipeline status")
-                //     }, 10000)
-                // } while (pipelineResponse.status !== RunningProcessStatus.COMPLETED)
-            })
+              })
         })
 
 
-        AvvirApi.files.listProjectFiles({projectId}, user).then((projectFiles) => {
-            expect(projectFiles.slice(-1)[0].url).to.be("An avvir gcs url?")
-
-        })
-
+    AvvirApi.files.listProjectFiles({projectId}, user).then((projectFiles) => {
+      expect(projectFiles.slice(-1)[0].url).to.be("An avvir gcs url?")
+      done()
     })
+
+  })
 });
