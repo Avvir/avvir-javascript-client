e2d2a423062ef119079d7e903fe64b11340de2f6
Jamel Cole <jamel@avvir.io>
2021-09-28 14:17:52 -0400

    Create intergration test for processing deviation analysis and progress analysis on scan dataset

--- source/models/enums/pipeline_types.ts
+++ source/models/enums/pipeline_types.ts
@@ -1,6 +1,17 @@
+import ApiPipeline from "../api/api_pipeline";
+
+export type PipelineName = "downsample-scan" |
+    "convert-ifc-and-process-scans" |
+    "generate-ifc" |
+    "create-and-process-svf" |
+    "download-and-zip-project-files-folder" |
+    "pipeline-steps" |
+    "ingest-external-photo-project-data" |
+    "ingest-project-file";
 
 export enum Pipelines {
-    INGEST_PROJECT_FILE = "ingest-project-file"
+    INGEST_PROJECT_FILE = "ingest-project-file",
+    PIPELINE_STEPS = "pipeline-steps"
 }
 
 export default Pipelines;
