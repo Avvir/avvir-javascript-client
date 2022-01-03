e9ab13a6f0ddd215e87c070d868e9f85a31f0cfe
Cameron Honis <cameron@avvir.io>
2022-01-03 16:11:24 -0500

    fix export, change package.json version

--- source/models/domain/index.ts
+++ source/models/domain/index.ts
@@ -1,7 +1,7 @@
 export {DeviatedDetailedElement, DetailedElement, Deviation, DeviationScanResult, ScanResult, isDeviationScanResult} from "./detailed_element";
 export {Masterformat} from "./masterformat";
 export {RunningProcess} from "./running_process";
-export {ViewCamera, View, ViewAttributes, ViewParameter, ViewCameraParameter, ViewFilters, ViewTrades, DeviationTolerance} from "./view";
+export {PhotoViewerDetails ,ViewCamera, View, ViewAttributes, ViewParameter, ViewCameraParameter, ViewFilters, ViewTrades, DeviationTolerance} from "./view";
 export {GatewayUser, FirebaseUser, User, BasicUser, isGatewayUser, isFirebaseUser} from "./user";
 
 export * from "./photos";
