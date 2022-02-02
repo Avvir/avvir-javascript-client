abfed70a845a43193b799fa13c86bc885d2ecaeb
Ben Groves <ben@avvir.io>
2022-02-02 12:38:04 -0500

    Fix Circle CI publish script [#180857952]

--- scripts/circle-ci-npm-publish.sh
+++ scripts/circle-ci-npm-publish.sh
@@ -4,6 +4,4 @@ if [[ "${CIRCLE_TAG}" =~ v[0-9]+(\.[0-9]+)* ]]; then
   npm set //registry.npmjs.org/:_authToken=$NPM_TOKEN
   npm --no-git-tag-version version ${CIRCLE_TAG}
   npm publish
-else
-  echo "Skipping publish, env var CIRCLE_TAG doesn't match."
 fi
