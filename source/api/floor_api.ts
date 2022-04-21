b00ffed700615d644c00cae5f63cc3ef7c23727c
Scott <scott@avvir.io>
2022-04-21 12:33:04 -0400

    Update types on floor api to mach what the gateway returns

--- source/api/floor_api.ts
+++ source/api/floor_api.ts
@@ -1,22 +1,22 @@
 // @ts-nocheck
 import ApiFloor from "../models/api/api_floor";
-import { User } from "../utilities/get_authorization_headers";
-import { AssociationIds } from "type_aliases";
 import Http from "../utilities/http";
 import makeErrorsPretty from "../utilities/make_errors_pretty";
+import { AssociationIds } from "type_aliases";
+import { User } from "../utilities/get_authorization_headers";
 
 export default class FloorApi {
-  static listFloorsForProject(projectId: string, user: User) : Promise<ApiFloor[]> {
+  static listFloorsForProject(projectId: string, user: User): Promise<ApiFloor[]> {
     let url = `${Http.baseUrl()}/projects/${projectId}/floors`;
     return Http.get(url, user);
   }
 
   static createFloor(projectId: string, floorNumber: string, user: User): Promise<ApiFloor> {
     const url = `${Http.baseUrl()}/projects/${projectId}/floors`;
-    return Http.post(url, user, {text: floorNumber});
+    return Http.post(url, user, { text: floorNumber });
   }
 
-  static getFloor({ projectId, floorId }: AssociationIds, user: User) : Promise<ApiFloor[]> {
+  static getFloor({ projectId, floorId }: AssociationIds, user: User): Promise<ApiFloor> {
     const url = `${Http.baseUrl()}/projects/${projectId}/floors/${floorId}`;
     return Http.get(url, user);
   }
@@ -26,16 +26,15 @@ export default class FloorApi {
     return Http.patch(url, user, floor);
   }
 
-  static updateFloorOrder({ projectId, floorId }: AssociationIds, ordinal: number, user: User):Promise<void> {
+  static updateFloorOrder({ projectId, floorId }: AssociationIds, ordinal: number, user: User): Promise<void> {
     const url = `${Http.baseUrl()}/projects/${projectId}/floors/${floorId}/reorder/${ordinal}`;
     return Http.patch(url, user, null);
   }
 
-  static deleteFloor({projectId, floorId}, user: User): Promise<ApiFloor[]> {
+  static deleteFloor({ projectId, floorId }, user: User): Promise<void> {
     const url = `${Http.baseUrl()}/projects/${projectId}/floors/${floorId}`;
     return Http.delete(url, user);
   }
-
 }
 
 makeErrorsPretty(FloorApi);
