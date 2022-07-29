ee6f62ebff8ec7e67fd4d0903cc4d3b323de23e6
Lisa van Gelder <lisa@avvir.io>
2022-07-29 09:03:32 -0400

    WIP - send trace id using b3 header to gateway

--- source/utilities/http.ts
+++ source/utilities/http.ts
@@ -3,8 +3,52 @@ import {httpGetHeaders, httpPostHeaders} from "./request_headers";
 import {isFirebaseUser, isGatewayUser} from "../models";
 import Config from "../config";
 
+import { context, trace } from '@opentelemetry/api';
+import { ConsoleSpanExporter, SimpleSpanProcessor } from '@opentelemetry/sdk-trace-base';
+import { WebTracerProvider } from '@opentelemetry/sdk-trace-web';
+import { getWebAutoInstrumentations } from '@opentelemetry/auto-instrumentations-web';
+import { ZoneContextManager } from '@opentelemetry/context-zone';
+import { B3Propagator } from '@opentelemetry/propagator-b3';
+import { registerInstrumentations } from '@opentelemetry/instrumentation';
+
+const provider = new WebTracerProvider();
+
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
+provider.register({
+  contextManager: new ZoneContextManager(),
+  propagator: new B3Propagator(),
+});
+
+
+registerInstrumentations({
+  instrumentations: [
+    getWebAutoInstrumentations({
+      // load custom configuration for xml-http-request instrumentation
+      // needs to be secured to API - need to make regex out of gateway URL in the config
+      '@opentelemetry/instrumentation-xml-http-request': {
+        propagateTraceHeaderCorsUrls: [
+          /http:\/\/localhost:8080\.*/,
+        ],
+      },
+      // load custom configuration for fetch instrumentation
+      // needs to be secured to api
+      '@opentelemetry/instrumentation-fetch': {
+        propagateTraceHeaderCorsUrls: [
+          // Config.getConfiguration().AVVIR_GATEWAY_URL,
+          /http:\/\/localhost:8080\.*/,
+        ],
+      },
+    }),
+  ],
+
+
+});
+
+const  webTracerWithZone = provider.getTracer('avvir-tracer-web');
+
 export default class Http {
   static baseUrl = (): string => Config.getConfiguration().AVVIR_GATEWAY_URL;
+
 
   static fetch(url: RequestInfo, data) {
     if (Config.getConfiguration().logFetch) {
