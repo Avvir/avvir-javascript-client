732c62f648f3852e07f3bd4dc22aa36c3dd84a4b
Ben Groves <ben@avvir.io>
2022-08-04 15:01:59 -0400

    Add deletePhotoLocations method to PhotosApi [#182374022]

--- source/models/api/api_photo_session.ts
+++ source/models/api/api_photo_session.ts
@@ -2,19 +2,22 @@ import addReadOnlyPropertiesToModel from "../../mixins/add_read_only_properties_
 import { DateLike, ModifyPartial } from "type_aliases";
 import addInstantGetterAndSetterToApiModel from "../../mixins/add_instant_getter_and_setter_to_api_model";
 
-export type ApiPhotoSessionArgs = ModifyPartial<ApiPhotoSession, { sessionDate?: DateLike }>
+export type ApiPhotoSessionArgs = ModifyPartial<ApiPhotoSession, {
+  sessionDate?: DateLike,
+  deletedAt?: DateLike
+}>
 
 export class ApiPhotoSession {
-  constructor({ id, photoAreaId, sessionDate }: ApiPhotoSessionArgs = {}) {
+  constructor({ id, photoAreaId, sessionDate, deletedAt }: ApiPhotoSessionArgs = {}) {
     addReadOnlyPropertiesToModel(this, { id, photoAreaId });
-    addInstantGetterAndSetterToApiModel(this, "sessionDate");
-    // @ts-ignore
-    this.sessionDate = sessionDate;
+    addInstantGetterAndSetterToApiModel(this, "sessionDate", sessionDate);
+    addInstantGetterAndSetterToApiModel(this, "deletedAt", deletedAt);
   }
 
   readonly id: number;
   readonly photoAreaId: number;
   readonly sessionDate: number;
+  readonly deletedAt: number;
 }
 
 export default ApiPhotoSession;
