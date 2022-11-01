98011c7e9dc73c94430e877537668017a01184ac
cam <cameron@avvir.io>
2022-11-01 10:59:08 -0500

    tweak triggerJobStepsPipeline return type

--- source/api/pipeline_api.ts
+++ source/api/pipeline_api.ts
@@ -2,14 +2,13 @@ import {AssociationIds} from "../models";
 import Http from "../utilities/http";
 import makeErrorsPretty from "../utilities/make_errors_pretty";
 import {User} from "../utilities/get_authorization_headers";
-import ApiArgoResponse from "../models/api/api_argo_response";
 import ApiPipeline, {ApiPipelineArgument} from "../models/api/api_pipeline";
 
 export default class PipelineApi {
-  static triggerJobStepsPipeline(associationIds: AssociationIds, body = {}, user: User): Promise<ApiArgoResponse> {
+  static triggerJobStepsPipeline(associationIds: AssociationIds, body = {}, user: User): Promise<ApiPipeline> {
     let {accountId, projectId, floorId, scanDatasetId} = associationIds;
     const url = `${Http.baseUrl()}/pipeline/${accountId}/${projectId}/${floorId}/${scanDatasetId}/trigger`;
-    return Http.post(url, user, body) as unknown as Promise<ApiArgoResponse>;
+    return Http.post(url, user, body) as unknown as Promise<ApiPipeline>;
   }
 
   static triggerPipeline(body: ApiPipelineArgument = {}, user: User): Promise<ApiPipeline> {
