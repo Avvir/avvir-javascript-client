647d7d61b0555366bd0d18f46667f2f7ecdb681c
Ben Groves <ben@avvir.io>
2023-02-23 11:33:32 -0500

    Add ProjectWorkPackageType for ApiWorkPackage [#184451980]

--- source/models/api/api_work_package.ts
+++ source/models/api/api_work_package.ts
@@ -1,9 +1,15 @@
+import {ProjectWorkPackageType} from "../enums";
+
 export class ApiWorkPackage {
     id?: number
     name: string
+    type?: ProjectWorkPackageType
+    masterformatCodes?: string[];
 
-    constructor({id, name} : Partial<ApiWorkPackage> = {}) {
+    constructor({id, name, type, masterformatCodes} : Partial<ApiWorkPackage> = {}) {
         this.id = id;
         this.name = name;
+        this.type = type;
+        this.masterformatCodes = masterformatCodes;
     }
 }
