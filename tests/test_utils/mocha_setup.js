080d9f7b1450f1abd15266c3fca8ed83da77fb0b
Scott Harrison <scott@avvir.io>
2023-04-24 14:03:15 -0400

    Add missing parameters to project api methods

--- tests/test_utils/mocha_setup.js
+++ tests/test_utils/mocha_setup.js
@@ -7,16 +7,3 @@ register({
   ignore: [/node_modules\/(?!three).*/, /node_modules\/three\/build\/(?!three\.module\.js)/],
   extensions: [".ts", ".tsx", ".js"]
 });
-
-function noop() {
-  return null;
-}
-
-// prevent mocha tests from breaking when trying to require a css file
-require.extensions[".css"] = noop;
-require.extensions[".png"] = noop;
-require.extensions[".jpg"] = noop;
-require.extensions[".jpeg"] = noop;
-require.extensions[".bmp"] = noop;
-require.extensions[".svg"] = noop;
-require.extensions[".obj"] = noop;
