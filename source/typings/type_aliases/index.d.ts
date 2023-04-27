1b80c722e4bb42548e929c7cb76509727181e1d8
Scott Harrison <scott@avvir.io>
2023-04-27 12:35:48 -0400

    Add endpoint for unarchive project
    - Add some missing tests
    - Refactor: change `accountId` to `organizationId` in all parameters
       - So that we consistently only accept `organizationId`

--- source/typings/type_aliases/index.d.ts
+++ source/typings/type_aliases/index.d.ts
@@ -1,26 +1,16 @@
-import firebase from "firebase";
-import {Action} from "redux";
-import {Matrix3, Matrix4} from "three";
-import {Moment} from "moment";
-import {ThunkAction, ThunkDispatch} from "redux-thunk";
-import {Action as RoutingAction, Meta, Query} from "redux-first-router";
+import { Action } from "redux";
+import { Matrix3, Matrix4 } from "three";
+import { Moment } from "moment";
+import { Action as RoutingAction, Meta, Query } from "redux-first-router";
 
 import ApiCloudFile from "../../models/api/api_cloud_file";
 import ApiMatrix4 from "../../models/api/api_matrix_4";
-import CloudFile from "../../models/domain/cloud_file";
-import ResponseError from "../../models/response_error";
-import CustomError from "../../models/custom_error";
-import {PurposeType} from "../../models/domain/enums/purpose_type";
-import {OutputParametricSelector} from "reselect";
-import {ReduxStore} from "../../services/reducers/root_reducer";
-import {SinonSpy, SinonStub} from "sinon";
+import { PurposeType } from "../../models/domain/enums/purpose_type";
 import ApiMatrix3 from "../../models/api/api_matrix_3";
 
 declare type DeepPartial<T> = T extends object ? { [P in keyof T]?: DeepPartial<T[P]>; } : T;
 declare type Modify<T, R> = Omit<T, keyof R> & R
 declare type ModifyPartial<T, R> = Partial<Omit<T, keyof R> & R>
-declare type Stubbed<T> = { [Key in keyof T]: T[Key] extends Function ? SinonStub : T[Key] }
-declare type Spied<T> = { [Key in keyof T]: T[Key] extends Function ? SinonSpy : T[Key] }
 
 declare type DateLike = Moment | Date | number | string | null;
 declare type Vector2Like = { x: number, y: number };
@@ -28,7 +18,6 @@ declare type Vector3Like = { x: number, y: number, z: number };
 declare type Vector4Like = { a: number, b: number, c: number, d: number };
 declare type Matrix3Like = ApiMatrix3 | Matrix3 | null
 declare type Matrix4Like = ApiMatrix4 | Matrix4 | null
-declare type ErrorLike = Error | ResponseError | { message: string }
 
 declare interface FileLike extends Partial<File> {
   path?: string,
@@ -46,17 +35,7 @@ declare interface FileLike extends Partial<File> {
 //   declare type Element = { projectId?: string, floorId?: string, scanDatasetId?: string, globalId: string }
 // }
 
-declare interface EventWithTarget<Target extends EventTarget = EventTarget> extends Event {
-  target: Target
-  currentTarget: Target
-}
-
-declare interface AvvirThunkAction<Return, State, Actions extends Action> extends ThunkAction<Return, State, unknown, Actions> {
-  (dispatch: ThunkDispatch<State, unknown, Actions>, getState?: () => State): Return
-}
-
 declare type AssociationIds = {
-  accountId?: string,
   commentThreadId?: number,
   commentId?: number,
   organizationId?: string,
@@ -72,15 +51,7 @@ declare type AssociationIds = {
   wbsCode?: string
 };
 
-declare type FirebaseUploadTask = firebase.storage.UploadTask
-declare type FirebaseRef = firebase.storage.Reference
-declare type ByFirebaseId<Model> = { [firebaseId: string]: Model }
-declare type ByDatabaseId<Model> = { [id: number]: Model }
-
-declare type AvvirFiles<Type extends PurposeType = PurposeType> = { [purposeType in Type]?: CloudFile[] }
 declare type AvvirApiFiles<Type extends PurposeType = PurposeType> = { [purposeType in Type]?: ApiCloudFile | ApiCloudFile[] }
-
-export type Dispatch<Event> = ThunkDispatch<ReduxStore, unknown, Event>;
 
 declare interface AvvirEvent<Type extends string, Payload> extends Action<Type> {
   payload: Payload
@@ -91,19 +62,4 @@ declare interface AvvirPayloadlessRoutingEvent<Type extends string> extends Rout
   meta?: Meta
   query?: Query
   navKey?: string | null
-}
-
-declare interface AvvirRoutingEvent<Type extends string, Payload> extends AvvirPayloadlessRoutingEvent<Type> {
-  payload: {
-    query?: Query
-  } & Payload
-}
-
-declare type AvvirErrorEvent<Type extends string, Err extends ErrorLike = ErrorLike> = AvvirEvent<Type, { error: Err }>
-declare type EventCreator<Type extends string, Payload> = (...args) => AvvirEvent<Type, Payload>
-
-declare type AvvirSelector<Return, InputSelectorReturns, Props = unknown> = OutputParametricSelector<ReduxStore, Props, Return, (...args: InputSelectorReturns) => Return>
-
-declare interface Constructor<Clazz, Args> {
-  new(...args: Args): Clazz
 }
