f286b72bc260ddf3837a54b8a06450e1225e198c
Trevor <trevor@avvir.io>
2023-07-28 19:28:23 -0400

    Remove job steps and project export endoints
     - These pipelines are accessed via the /pipelines endpoint

--- source/api/pipeline_api.ts
+++ source/api/pipeline_api.ts
@@ -5,20 +5,8 @@ import { User } from "../utilities/get_authorization_headers";
 import ApiPipeline, { ApiPipelineArgument } from "../models/api/api_pipeline";
 
 export default class PipelineApi {
-  static triggerJobStepsPipeline(associationIds: AssociationIds, body = {}, user: User): Promise<ApiPipeline> {
-    const {organizationId, projectId, floorId, scanDatasetId} = associationIds;
-    const url = `${Http.baseUrl()}/pipeline/${organizationId}/${projectId}/${floorId}/${scanDatasetId}/trigger`;
-    return Http.post(url, user, body) as unknown as Promise<ApiPipeline>;
-  }
-
-  static triggerPipeline(body: ApiPipelineArgument = {}, user: User): Promise<ApiPipeline> {
+    static triggerPipeline(body: ApiPipelineArgument = {}, user: User): Promise<ApiPipeline> {
     const url = `${Http.baseUrl()}/pipelines`;
-    return Http.post(url, user, body) as unknown as Promise<ApiPipeline>;
-  }
-
-  static triggerIngestProjectReportPipeline(associationIds: AssociationIds, body = {}, user: User): Promise<ApiPipeline> {
-    let {organizationId, projectId} = associationIds;
-    const url = `${Http.baseUrl()}/pipeline/${organizationId}/${projectId}/ingest-project-export-report/trigger`;
     return Http.post(url, user, body) as unknown as Promise<ApiPipeline>;
   }
 
--- tests/api/pipeline_api_test.ts
+++ tests/api/pipeline_api_test.ts
@@ -1,15 +1,15 @@
-import { sandbox } from "../test_utils/setup_tests";
-import { expect } from "chai";
+import {sandbox} from "../test_utils/setup_tests";
+import {expect} from "chai";
 import fetchMock from "fetch-mock";
-import _, { Dictionary } from "underscore";
+import _, {Dictionary} from "underscore";
 
 import PipelineApi from "../../source/api/pipeline_api";
-import { FIREBASE, GATEWAY_JWT } from "../../source/models/enums/user_auth_type";
+import {FIREBASE, GATEWAY_JWT} from "../../source/models/enums/user_auth_type";
 import Config from "../../source/config";
 import Http from "../../source/utilities/http";
 import ApiPipeline from "../../source/models/api/api_pipeline";
 import RunningProcessStatus from "../../source/models/enums/running_process_status";
-import { USER } from "../../source/models/enums/user_role";
+import {USER} from "../../source/models/enums/user_role";
 
 describe("PipelineApi", () => {
   let user;
@@ -18,198 +18,164 @@ describe("PipelineApi", () => {
     user = {authType: FIREBASE, firebaseUser: {idToken: "some-firebase.id.token"}};
 
   });
-
-  describe("#triggerJobStepsPipeline", () => {
+  describe("#triggerPipeline", () => {
     beforeEach(() => {
-      fetchMock.post(`${Http.baseUrl()}/pipeline/some-organization-id/some-project-id/some-floor-id/some-scan-dataset-id/trigger`, 200);
+      fetchMock.post(`${Http.baseUrl()}/pipelines`, (url, req) => {
+        const {name, id} = req.body;
+        const apiResponse = new ApiPipeline({
+          status: RunningProcessStatus.RUNNING,
+          name,
+          id
+        });
+        return {
+          status: 200,
+          body: apiResponse,
+          headers: {"ContentType": "application/json"}
+        }
+      });
     });
-
     it("Calls the correct URL", () => {
-      PipelineApi.triggerJobStepsPipeline({
-          organizationId: "some-organization-id",
-          projectId: "some-project-id",
-          floorId: "some-floor-id",
-          scanDatasetId: "some-scan-dataset-id"
-        },
-        {},
-        { authType:FIREBASE, firebaseUser: {idToken: "some-firebase.id.token" }});
-
-      expect(fetchMock.lastUrl()).to.eq(
-        `${Http.baseUrl()}/pipeline/some-organization-id/some-project-id/some-floor-id/some-scan-dataset-id/trigger`
+      // id: 1,
+      // name: "some-name",
+      // externalId: "some-external-id",
+      // externalUrl: "someUrl.com",
+      // firebaseFloorId: "some-floor-id",
+      // firebaseProjectId: "some-project-id",
+      // firebaseScanDatasetId: "some-dataset-id",
+      // status: RunningProcessStatus.RUNNING
+      PipelineApi.triggerPipeline(
+          new ApiPipeline({}),
+          {authType: FIREBASE, firebaseUser: {idToken: "some-firebase.id.token"}}
       );
-    });
-    describe("#triggerPipeline", () => {
-      beforeEach(() => {
-        fetchMock.post(`${Http.baseUrl()}/pipelines`,(url, req)=>{
-          const { name, id } = req.body;
-          const apiResponse = new ApiPipeline({
-            status: RunningProcessStatus.RUNNING,
-            name,
-            id
-          });
-          return {
-            status: 200,
-            body: apiResponse,
-            headers: {"ContentType": "application/json"}
-          }
-        });
-      });
-      it("Calls the correct URL", () => {
-        // id: 1,
-        // name: "some-name",
-        // externalId: "some-external-id",
-        // externalUrl: "someUrl.com",
-        // firebaseFloorId: "some-floor-id",
-        // firebaseProjectId: "some-project-id",
-        // firebaseScanDatasetId: "some-dataset-id",
-        // status: RunningProcessStatus.RUNNING
-        PipelineApi.triggerPipeline(
-            new ApiPipeline({}),
-            { authType:FIREBASE, firebaseUser: {idToken: "some-firebase.id.token" }}
-        );
-        expect(fetchMock.lastUrl()).to.eq(`${Http.baseUrl()}/pipelines`);
-      });
-
-      it("Returns an ApiPipeline type",  () => {
-        let request = new ApiPipeline({
-          name: "pipeline-steps",
-          id: 1
-        });
-        PipelineApi.triggerPipeline(
-            request,
-            { authType:FIREBASE, firebaseUser: {idToken: "some-firebase.id.token" }}
-        )
-
-        const lastCall = fetchMock.lastCall()[1];
-
-        expect(lastCall.body).to.be.eq(JSON.stringify(request));
-        expect((lastCall.headers as Dictionary<string>).Accept).to.be.eq("application/json");
-
-      })
-
+      expect(fetchMock.lastUrl()).to.eq(`${Http.baseUrl()}/pipelines`);
     });
 
-
-    describe("::checkPipelineStatus", () => {
-      beforeEach(() => {
-        fetchMock.get(`${Http.baseUrl()}/pipelines/10`, {...new ApiPipeline({id: 10, name: "pipeline-steps"})});
+    it("Returns an ApiPipeline type", () => {
+      let request = new ApiPipeline({
+        name: "pipeline-steps",
+        id: 1
       });
+      PipelineApi.triggerPipeline(
+          request,
+          {authType: FIREBASE, firebaseUser: {idToken: "some-firebase.id.token"}}
+      )
 
-      it("makes a request to the gateway", () => {
-        PipelineApi.checkPipelineStatus({projectId: "some-project-id"}, 10, {
-          authType: GATEWAY_JWT,
-          gatewayUser: {idToken: "some-firebase.id.token", role: USER}
-        });
+      const lastCall = fetchMock.lastCall()[1];
 
-        expect(fetchMock.lastCall()[0]).to.eq(`${Http.baseUrl()}/pipelines/10`);
-        expect(fetchMock.lastOptions().headers.Accept).to.eq("application/json");
-      });
+      expect(lastCall.body).to.be.eq(JSON.stringify(request));
+      expect((lastCall.headers as Dictionary<string>).Accept).to.be.eq("application/json");
 
-      it("includes the authorization headers", () => {
-        PipelineApi.checkPipelineStatus({projectId: "some-project-id"}, 10, {
-          authType: GATEWAY_JWT,
-          gatewayUser: {idToken: "some-firebase.id.token", role: USER}
-        });
+    })
+  });
 
-        expect(fetchMock.lastOptions().headers.Authorization).to.eq("Bearer some-firebase.id.token");
-      });
+
+  describe("::checkPipelineStatus", () => {
+    beforeEach(() => {
+      fetchMock.get(`${Http.baseUrl()}/pipelines/10`, {...new ApiPipeline({id: 10, name: "pipeline-steps"})});
     });
 
-    describe("when the pipeline fails to trigger", () => {
-      beforeEach(() => {
-        const alreadyRunningResponse = {
-          body: {
-            "jobLogs": "Started by remote host 10.100.0.16" +
+    it("makes a request to the gateway", () => {
+      PipelineApi.checkPipelineStatus({projectId: "some-project-id"}, 10, {
+        authType: GATEWAY_JWT,
+        gatewayUser: {idToken: "some-firebase.id.token", role: USER}
+      });
+
+      expect(fetchMock.lastCall()[0]).to.eq(`${Http.baseUrl()}/pipelines/10`);
+      expect(fetchMock.lastOptions().headers.Accept).to.eq("application/json");
+    });
+
+    it("includes the authorization headers", () => {
+      PipelineApi.checkPipelineStatus({projectId: "some-project-id"}, 10, {
+        authType: GATEWAY_JWT,
+        gatewayUser: {idToken: "some-firebase.id.token", role: USER}
+      });
+
+      expect(fetchMock.lastOptions().headers.Authorization).to.eq("Bearer some-firebase.id.token");
+    });
+  });
+
+  describe("when the pipeline fails to trigger", () => {
+    beforeEach(() => {
+      const alreadyRunningResponse = {
+        body: {
+          "jobLogs": "Started by remote host 10.100.0.16" +
               "\r\n[Pipeline] readJSON" +
               "\r\n[Pipeline] podTemplate" +
               "\r\n[Pipeline] {" +
               "\r\n[Pipeline] node" +
               "\r\nStill waiting to schedule task" +
               "\r\nWaiting for next available executor",
-            "error": "Pipeline job already running",
-            "errorDetails": "Jenkins job number: 4"
-          },
-          status: 401,
-          headers: {"ContentType": "application/json"}
+          "error": "Pipeline job already running",
+          "errorDetails": "Jenkins job number: 4"
+        },
+        status: 401,
+        headers: {"ContentType": "application/json"}
 
-        };
+      };
 
-        const filesMissingResponse = {
-          body: {
-            "message": "Cannot trigger pipeline job for this scanDataset",
-          },
-          status: 401,
-          headers: {"ContentType": "application/json"}
-        };
+      const filesMissingResponse = {
+        body: {
+          "message": "Cannot trigger pipeline job for this scanDataset",
+        },
+        status: 401,
+        headers: {"ContentType": "application/json"}
+      };
 
-        fetchMock.mock(`begin:${Http.baseUrl()}/pipeline/`, (url) => {
-          const startLength = `${Http.baseUrl()}/pipeline/`.length;
-          const argsString = url.slice(startLength).split("/");
-          const floorId = argsString[2];
-          const scanDatasetId = argsString[3];
+      fetchMock.mock(`begin:${Http.baseUrl()}/pipelines`, (url) => {
+        const startLength = `${Http.baseUrl()}/pipelines`.length;
+        const argsString = url.slice(startLength).split("/");
+        const floorId = argsString[2];
+        const scanDatasetId = argsString[3];
 
-          if (scanDatasetId === "some-scan-dataset-with-an-already-running-job") {
-            return alreadyRunningResponse;
-          } else if (floorId === "some-non-existent-floor-id") {
-            return filesMissingResponse;
-          } else {
-            return {
-                status: 500,
-                body: {message: "some unfortunate error"},
-                headers: {"ContentType": "application/json"}
-            };
-          }
-        }, {overwriteRoutes: true});
-      });
-
-      it("rejects the promise and calls the shared error handler", () => {
-        fetchMock.mock(`begin:${Http.baseUrl()}/pipeline/`, () => {
+        if (scanDatasetId === "some-scan-dataset-with-an-already-running-job") {
+          return alreadyRunningResponse;
+        } else if (floorId === "some-non-existent-floor-id") {
+          return filesMissingResponse;
+        } else {
           return {
-              status: 500,  body: {message: "some unfortunate error"},
-              headers: {"ContentType": "application/json"}
-          }
-        }, {overwriteRoutes: true});
+            status: 500,
+            body: {message: "some unfortunate error"},
+            headers: {"ContentType": "application/json"}
+          };
+        }
+      }, {overwriteRoutes: true});
+    });
 
-        sandbox.spy(Config, "sharedErrorHandler");
+    it("rejects the promise and calls the shared error handler", () => {
+      fetchMock.mock(`begin:${Http.baseUrl()}/pipeline/`, () => {
+        return {
+          status: 500, body: {message: "some unfortunate error"},
+          headers: {"ContentType": "application/json"}
+        }
+      }, {overwriteRoutes: true});
 
-        return PipelineApi.triggerJobStepsPipeline({
-            accountId: "some-organization-id",
-            projectId: "some-project-id",
-            floorId: "some-floor-id",
-            scanDatasetId: "some-scan-id"
-          },
-          {},
+      sandbox.spy(Config, "sharedErrorHandler");
+
+      return PipelineApi.triggerPipeline(new ApiPipeline({}),
           {authType: FIREBASE, firebaseUser: {idToken: "some-firebase.id.token"}}
-        ).catch((err) => {
-          return err;
-        }).then((err) => {
-          expect(err.status).to.eql(500);
-          expect(err.message).to.eq("some unfortunate error");
-          expect(Config.sharedErrorHandler).to.have.been.calledWithMatch({
-            error: err,
-            action: "triggerJobStepsPipeline",
-            arguments: [{
-              accountId: "some-organization-id",
-              projectId: "some-project-id",
-              floorId: "some-floor-id",
-              scanDatasetId: "some-scan-id"
-            },
-              {},
-              {authType: FIREBASE, firebaseUser: {idToken: "some-firebase.id.token"}}]
-          });
+      ).catch((err) => {
+        return err;
+      }).then((err) => {
+        expect(err.status).to.eql(500);
+        expect(err.message).to.eq("some unfortunate error");
+        expect(Config.sharedErrorHandler).to.have.been.calledWithMatch({
+          error: err,
+          action: "triggerPipeline",
+          arguments: [new ApiPipeline(),
+            {authType: FIREBASE, firebaseUser: {idToken: "some-firebase.id.token"}}]
         });
       });
+    });
 
-      it("handles missing file errors", () => {
-        sandbox.stub(Config, "sharedErrorHandler");
+    it("handles missing file errors", () => {
+      sandbox.stub(Config, "sharedErrorHandler");
 
-        return PipelineApi.triggerJobStepsPipeline({
-            accountId: "some-organization-id",
-            projectId: "some-project-id",
-            floorId: "some-non-existent-floor-id",
-            scanDatasetId: "some-scan-dataset-id"
-          },
-          {},
+      return PipelineApi.triggerPipeline(new ApiPipeline({
+            firebaseClientAccountId: "some-organization-id",
+            firebaseProjectId: "some-project-id",
+            firebaseFloorId: "some-non-existent-floor-id",
+            firebaseScanDatasetId: "some-scan-dataset-id"
+          }),
           {authType: FIREBASE, firebaseUser: {idToken: "some-firebase.id.token"}})
           .catch(_.noop)
           .finally(() => {
@@ -217,25 +183,24 @@ describe("PipelineApi", () => {
               // type: API_FAILURE,
             });
           });
-      });
+    });
 
-      it("handles pipeline already running errors", () => {
-        sandbox.stub(Config, "sharedErrorHandler");
-        return PipelineApi.triggerJobStepsPipeline({
-            accountId: "some-organization-id",
-            projectId: "some-project-id",
-            floorId: "some-floor-id",
-            scanDatasetId: "some-scan-dataset-with-an-already-running-job"
-          },
+    it("handles pipeline already running errors", () => {
+      sandbox.stub(Config, "sharedErrorHandler");
+      return PipelineApi.triggerPipeline(new ApiPipeline({
+            firebaseClientAccountId: "some-organization-id",
+            firebaseProjectId: "some-project-id",
+            firebaseFloorId: "some-floor-id",
+            firebaseScanDatasetId: "some-scan-dataset-with-an-already-running-job"
+          }),
           {},
           {authType: FIREBASE, firebaseUser: {idToken: "some-firebase.id.token"}})
           .catch(_.noop)
           .finally(() => {
             expect(Config.sharedErrorHandler).to.have.been.calledWithMatch({
-             // TODO
+              // TODO
             });
           });
-      });
     });
   });
 });
