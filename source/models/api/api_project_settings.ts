fc4f953073a5dd47550a6da90e36fbca93262e9d
Sharon Zhu <sharonzhu@avvir.io>
2023-09-14 13:57:54 -0400

    Add inspectReports and budget5d to project settings

--- source/models/api/api_project_settings.ts
+++ source/models/api/api_project_settings.ts
@@ -2,6 +2,8 @@ export class ApiProjectSettings {
   projectSummaryPage: boolean;
   progressReportTab: boolean;
   photosOnly: boolean;
+  inspectReports: boolean;
+  budget5d: boolean;
 }
 
 export default ApiProjectSettings;
