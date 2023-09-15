8ba779f4dc3df7e3fe1836bb2a522b1aa9ea9931
Sharon Zhu <sharonzhu@avvir.io>
2023-09-15 18:09:04 -0400

    remove option to fetch inspect entries when listing inspect reports

--- source/api/report_api.ts
+++ source/api/report_api.ts
@@ -9,11 +9,8 @@ export default class ReportApi {
         let url = `${Http.baseUrl()}/projects/${projectId}/reports`;
         return Http.post(url, user, report) as unknown as Promise<ApiInspectReport>;
     }
-    static listInspectReports({projectId}: AssociationIds, user: User, displayEntries?: boolean): Promise<ApiInspectReport[]> {
+    static listInspectReports({projectId}: AssociationIds, user: User): Promise<ApiInspectReport[]> {
         let url = `${Http.baseUrl()}/projects/${projectId}/reports`;
-        if (displayEntries) {
-            url += "?displayEntries=true";
-        }
         return Http.get(url, user) as unknown as Promise<ApiInspectReport[]>;
     }
     static listInspectReportEntries({projectId, inspectReportId}: AssociationIds, user: User): Promise<ApiInspectReportEntry[]> {
