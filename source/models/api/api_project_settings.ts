a9749fb2dd4d04595ba890108b6c6c9239aac6b7
Sharon Zhu <sharonzhu@avvir.io>
2023-10-11 11:42:06 -0400

    Add autoCalculateProgress to project settings api object

--- source/models/api/api_project_settings.ts
+++ source/models/api/api_project_settings.ts
@@ -2,10 +2,10 @@ export class ApiProjectSettings {
   projectSummaryPage: boolean;
   progressReportTab: boolean;
   photosOnly: boolean;
-  budget5d: boolean;
   inspectReports: boolean;
   inspect: boolean;
   progress4d: boolean;
   progress5d: boolean;
   autoScanAnalysis: boolean;
+  autoCalculateProgress: boolean;
 }
