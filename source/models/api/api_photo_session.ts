f009bf4b008f6cc49077641d941bac8c6cde014b
Sharon Zhu <sharonzhu@avvir.io>
2023-10-18 14:35:19 -0400

    Add external id to ApiPhotoSession [Fix]

--- source/models/api/api_photo_session.ts
+++ source/models/api/api_photo_session.ts
@@ -5,19 +5,22 @@ import addInstantGetterAndSetterToApiModel from "../../mixins/add_instant_getter
 export type ApiPhotoSessionArgs = ModifyPartial<ApiPhotoSession, {
   sessionDate?: DateLike,
   deletedAt?: DateLike
+  externalId?: string
 }>
 
 export class ApiPhotoSession {
-  constructor({ id, photoAreaId, sessionDate, deletedAt }: ApiPhotoSessionArgs = {}) {
+  constructor({ id, photoAreaId, sessionDate, deletedAt, externalId }: ApiPhotoSessionArgs = {}) {
     addReadOnlyPropertiesToModel(this, { id, photoAreaId });
     addInstantGetterAndSetterToApiModel(this, "sessionDate", sessionDate);
     addInstantGetterAndSetterToApiModel(this, "deletedAt", deletedAt);
+    this.externalId = externalId;
   }
 
   readonly id: number;
   readonly photoAreaId: number;
   readonly sessionDate: number;
   readonly deletedAt: number;
+  externalId: string;
 }
 
 export default ApiPhotoSession;
