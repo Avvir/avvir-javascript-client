db868888517076399b05850b6fc6d5f54955ab55
Scott Harrison <scott@avvir.io>
2023-10-19 14:13:15 -0400

    Add ordinal to api work package model
    [Fix]
    [#185846520]

--- source/models/api/api_work_package.ts
+++ source/models/api/api_work_package.ts
@@ -1,14 +1,16 @@
-import {ProjectWorkPackageType} from "../enums";
+import { ProjectWorkPackageType } from "../enums";
 
 export class ApiWorkPackage {
     id?: number
     name: string
+    ordinal: number
     type?: ProjectWorkPackageType
     masterformatCodes?: string[];
 
-    constructor({id, name, type, masterformatCodes} : Partial<ApiWorkPackage> = {}) {
+    constructor({id, name, ordinal, type, masterformatCodes} : Partial<ApiWorkPackage> = {}) {
         this.id = id;
         this.name = name;
+        this.ordinal = ordinal;
         this.type = type;
         this.masterformatCodes = masterformatCodes;
     }
