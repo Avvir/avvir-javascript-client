5341244dedbab548894727241b08457df45c8104
Scott Harrison <scott@avvir.io>
2024-08-07 12:54:37 -0400

    Add missing endpoints for project areas and project area work packages
    - Mark endpoints that take a different id now as deprecated
    
    [BSI-177]

--- tests/api/project_summary_api_test.ts
+++ tests/api/project_summary_api_test.ts
@@ -93,6 +93,54 @@ describe("ProjectSummaryApi", () => {
     })
   });
 
+  describe("::updateProjectArea", () => {
+    beforeEach(() => {
+      fetchMock.patch(`${Http.baseUrl()}/projects/some-project-id/areas/12`, new ApiProjectArea({
+        id: 12,
+        modelElementId: 1,
+        floorId: 7,
+        firebaseFloorId: "some-floor-id"
+      }));
+    });
+
+    it("makes a call to the endpoint", () => {
+      ProjectSummaryApi.updateProjectArea("some-project-id", 12, {
+        id: 12,
+        modelElementId: 1,
+        floorId: 7,
+        firebaseFloorId: "some-floor-id",
+        progress: [],
+        workPackages: []
+      }, user);
+
+      const fetchCall = fetchMock.lastCall();
+
+      expect(fetchCall[0]).to.eq(`${Http.baseUrl()}/projects/some-project-id/areas/12`);
+      expect(fetchMock.lastOptions().headers.Accept).to.eq("application/json");
+      expect(fetchMock.lastOptions().headers["Content-Type"]).to.eq("application/json");
+    });
+
+    it("sends the request with an Authorization header", () => {
+      ProjectSummaryApi.updateProjectArea(
+        "some-project-id",
+        12,
+        {
+          id: 12,
+          modelElementId: 1,
+          floorId: 7,
+          firebaseFloorId: "some-floor-id",
+          progress: [],
+          workPackages: []
+        },
+        {
+          authType: UserAuthType.GATEWAY_JWT,
+          gatewayUser: { idToken: "some-firebase.id.token", role: UserRole.USER }
+        });
+
+      expect(fetchMock.lastOptions().headers.Authorization).to.eq("Bearer some-firebase.id.token");
+    })
+  });
+
   describe("::updateProjectAreaProgress", () => {
     let projectAreaId, response;
     beforeEach(() => {
@@ -172,6 +220,130 @@ describe("ProjectSummaryApi", () => {
     });
   });
 
+  describe("::createProjectAreaWorkPackage", () => {
+    beforeEach(() => {
+      fetchMock.post(`${Http.baseUrl()}/projects/some-project-id/areas/12/work-packages`, new ApiProjectAreaWorkPackage({
+        id: 54,
+        projectWorkPackageId: 5,
+        workPackageId: 3,
+        status: "started",
+        expectedStart: null,
+        expectedCompletion: null,
+        start: null,
+        completion: null
+      }));
+    });
+
+    it("makes a call to the endpoint", () => {
+      ProjectSummaryApi.createProjectAreaWorkPackage(
+        "some-project-id",
+        12,
+        {
+            id: 54,
+            projectAreaId: 12,
+            projectWorkPackageId: 5,
+            workPackageId: 3,
+            status: "started",
+            expectedStart: null,
+            expectedCompletion: null,
+            start: null,
+            completion: null
+          },
+        user);
+
+      const fetchCall = fetchMock.lastCall();
+
+      expect(fetchCall[0]).to.eq(`${Http.baseUrl()}/projects/some-project-id/areas/12/work-packages`);
+      expect(fetchMock.lastOptions().headers.Accept).to.eq("application/json");
+      expect(fetchMock.lastOptions().headers["Content-Type"]).to.eq("application/json");
+    });
+
+    it("sends the request with an Authorization header", () => {
+      ProjectSummaryApi.createProjectAreaWorkPackage(
+        "some-project-id",
+        12,
+        {
+            id: 54,
+            projectAreaId: 12,
+            projectWorkPackageId: 5,
+            workPackageId: 3,
+            status: "started",
+            expectedStart: null,
+            expectedCompletion: null,
+            start: null,
+            completion: null
+          },
+        {
+          authType: UserAuthType.GATEWAY_JWT,
+          gatewayUser: { idToken: "some-firebase.id.token", role: UserRole.USER }
+        });
+
+      expect(fetchMock.lastOptions().headers.Authorization).to.eq("Bearer some-firebase.id.token");
+    });
+  });
+
+  describe("::updateProjectAreaWorkPackages", () => {
+    beforeEach(() => {
+      fetchMock.patch(`${Http.baseUrl()}/projects/some-project-id/areas/12/work-packages`, [new ApiProjectAreaWorkPackage({
+        id: 54,
+        projectWorkPackageId: 5,
+        workPackageId: 3,
+        status: "started",
+        expectedStart: null,
+        expectedCompletion: null,
+        start: null,
+        completion: null
+      })]);
+    });
+
+    it("makes a call to the endpoint", () => {
+      ProjectSummaryApi.updateProjectAreaWorkPackages(
+        "some-project-id",
+        12,
+        [{
+            id: 54,
+            projectAreaId: 12,
+            projectWorkPackageId: 5,
+            workPackageId: 3,
+            status: "started",
+            expectedStart: null,
+            expectedCompletion: null,
+            start: null,
+            completion: null
+          }],
+        user);
+
+      const fetchCall = fetchMock.lastCall();
+
+      expect(fetchCall[0]).to.eq(`${Http.baseUrl()}/projects/some-project-id/areas/12/work-packages`);
+      expect(fetchMock.lastOptions().headers.Accept).to.eq("application/json");
+      expect(fetchMock.lastOptions().headers["Content-Type"]).to.eq("application/json");
+    });
+
+    it("sends the request with an Authorization header", () => {
+      ProjectSummaryApi.updateProjectAreaWorkPackages(
+        "some-project-id",
+        12,
+        [{
+            id: 54,
+            projectAreaId: 12,
+            projectWorkPackageId: 5,
+            workPackageId: 3,
+            status: "started",
+            expectedStart: null,
+            expectedCompletion: null,
+            start: null,
+            completion: null
+          }],
+        {
+          authType: UserAuthType.GATEWAY_JWT,
+          gatewayUser: { idToken: "some-firebase.id.token", role: UserRole.USER }
+        });
+
+      expect(fetchMock.lastOptions().headers.Authorization).to.eq("Bearer some-firebase.id.token");
+    });
+  });
+
   describe("::getProjectArea", () => {
     let projectAreaId, response;
     beforeEach(() => {
@@ -211,6 +383,106 @@ describe("ProjectSummaryApi", () => {
       ProjectSummaryApi.getProjectArea(
         "some-project-id",
         projectAreaId,
+        {
+          authType: UserAuthType.GATEWAY_JWT,
+          gatewayUser: { idToken: "some-firebase.id.token", role: UserRole.USER }
+        });
+
+      expect(fetchMock.lastOptions().headers.Authorization).to.eq("Bearer some-firebase.id.token");
+    });
+  });
+
+  describe("::listProjectAreas", () => {
+    beforeEach(() => {
+      fetchMock.get(`${Http.baseUrl()}/projects/some-project-id/areas`, [new ApiProjectArea({
+        id: 12,
+        modelElementId: 3,
+        workPackages: [],
+        progress: []
+      })]);
+    });
+
+    it("makes a call to the endpoint", () => {
+      ProjectSummaryApi.listProjectAreas(
+        "some-project-id",
+        user);
+
+      const fetchCall = fetchMock.lastCall();
+
+      expect(fetchCall[0]).to.eq(`${Http.baseUrl()}/projects/some-project-id/areas`);
+      expect(fetchMock.lastOptions().headers.Accept).to.eq("application/json");
+    });
+
+    it("sends the request with an Authorization header", () => {
+      ProjectSummaryApi.listProjectAreas(
+        "some-project-id",
+        {
+          authType: UserAuthType.GATEWAY_JWT,
+          gatewayUser: { idToken: "some-firebase.id.token", role: UserRole.USER }
+        });
+
+      expect(fetchMock.lastOptions().headers.Authorization).to.eq("Bearer some-firebase.id.token");
+    });
+  });
+
+  describe("::listProjectAreaWorkPackages", () => {
+    beforeEach(() => {
+      fetchMock.get(`${Http.baseUrl()}/projects/some-project-id/areas/7/work-packages`, [new ApiProjectAreaWorkPackage({
+        id: 12,
+        projectAreaId: 7
+      })]);
+    });
+
+    it("makes a call to the endpoint", () => {
+      ProjectSummaryApi.listProjectAreaWorkPackages(
+        "some-project-id",
+        7,
+        user);
+
+      const fetchCall = fetchMock.lastCall();
+
+      expect(fetchCall[0]).to.eq(`${Http.baseUrl()}/projects/some-project-id/areas/7/work-packages`);
+      expect(fetchMock.lastOptions().headers.Accept).to.eq("application/json");
+    });
+
+    it("sends the request with an Authorization header", () => {
+      ProjectSummaryApi.listProjectAreaWorkPackages(
+        "some-project-id",
+        7,
+        {
+          authType: UserAuthType.GATEWAY_JWT,
+          gatewayUser: { idToken: "some-firebase.id.token", role: UserRole.USER }
+        });
+
+      expect(fetchMock.lastOptions().headers.Authorization).to.eq("Bearer some-firebase.id.token");
+    });
+  });
+
+  describe("::listProjectAreaWorkPackagesForProject", () => {
+    beforeEach(() => {
+      fetchMock.get(`${Http.baseUrl()}/projects/some-project-id/areas/work-packages`, [new ApiProjectAreaWorkPackage({
+        id: 12,
+        projectAreaId: 3
+      }), new ApiProjectAreaWorkPackage({
+        id: 13,
+        projectAreaId: 4
+      })]);
+    });
+
+    it("makes a call to the endpoint", () => {
+      ProjectSummaryApi.listProjectAreaWorkPackagesForProject(
+        "some-project-id",
+        user);
+
+      const fetchCall = fetchMock.lastCall();
+
+      expect(fetchCall[0]).to.eq(`${Http.baseUrl()}/projects/some-project-id/areas/work-packages`);
+      expect(fetchMock.lastOptions().headers.Accept).to.eq("application/json");
+    });
+
+    it("sends the request with an Authorization header", () => {
+      ProjectSummaryApi.listProjectAreaWorkPackagesForProject(
+        "some-project-id",
         {
           authType: UserAuthType.GATEWAY_JWT,
           gatewayUser: { idToken: "some-firebase.id.token", role: UserRole.USER }
