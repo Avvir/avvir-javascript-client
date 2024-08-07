e9c4ecc40e3411dc73c601fffe17202779b770bc
Scott Harrison <scott@avvir.io>
2024-08-07 12:54:37 -0400

    Add missing endpoints for project areas and project area work packages
    - Mark endpoints that take a different id now as deprecated
    
    [BSI-177]

--- source/api/project_summary_api.ts
+++ source/api/project_summary_api.ts
@@ -1,5 +1,5 @@
 import makeErrorsPretty from "../utilities/make_errors_pretty";
-import { ApiProjectArea, ApiProjectSummary, User } from "../models";
+import { ApiProjectArea, ApiProjectAreaWorkPackage, ApiProjectSummary, User } from "../models";
 import Http from "../utilities/http";
 
 export default class ProjectSummaryApi {
@@ -7,20 +7,50 @@ export default class ProjectSummaryApi {
         return Http.get(`${Http.baseUrl()}/projects/${projectId}/summary`, user) as unknown as Promise<ApiProjectSummary>;
     }
 
+    /**
+     * @deprecated Use {@link createProjectArea} or {@link updateProjectArea} instead
+     */
     static createOrReplaceProjectArea(projectId: string, modelElementId: number, projectArea: ApiProjectArea, user: User): Promise<ApiProjectArea> {
         return Http.put(`${Http.baseUrl()}/projects/${projectId}/areas/${modelElementId}`, user, projectArea) as unknown as Promise<ApiProjectArea>
     };
 
+    static createProjectArea(projectId: string, projectArea: ApiProjectArea, user: User): Promise<ApiProjectArea> {
+        return Http.post(`${Http.baseUrl()}/projects/${projectId}/areas`, user, projectArea) as unknown as Promise<ApiProjectArea>;
+    }
+
+    static updateProjectArea(projectId: string, projectAreaId: number, projectArea: ApiProjectArea, user: User): Promise<ApiProjectArea> {
+        return Http.patch(`${Http.baseUrl()}/projects/${projectId}/areas/${projectAreaId}`, user, projectArea) as unknown as Promise<ApiProjectArea>
+    };
+
+    /**
+     * @deprecated Use {@link updateProjectArea} to update the project area or {@link updateProjectAreaWorkPackages} to update the project area work packages
+     */
     static updateProjectAreaProgress(projectId: string, modelElementId: number, projectArea: ApiProjectArea, user: User): Promise<ApiProjectArea> {
         return Http.patch(`${Http.baseUrl()}/projects/${projectId}/areas/${modelElementId}`, user, projectArea) as unknown as Promise<ApiProjectArea>
     };
+
+    static createProjectAreaWorkPackage(projectId: string, projectAreaId: number, projectAreaWorkPackage: ApiProjectAreaWorkPackage, user: User): Promise<ApiProjectAreaWorkPackage> {
+        return Http.post(`${Http.baseUrl()}/projects/${projectId}/areas/${projectAreaId}/work-packages`, user, projectAreaWorkPackage) as unknown as Promise<ApiProjectAreaWorkPackage>;
+    }
+
+    static updateProjectAreaWorkPackages(projectId: string, projectAreaId: number, projectAreaWorkPackages: ApiProjectAreaWorkPackage[], user: User): Promise<ApiProjectAreaWorkPackage[]> {
+        return Http.patch(`${Http.baseUrl()}/projects/${projectId}/areas/${projectAreaId}/work-packages`, user, projectAreaWorkPackages) as unknown as Promise<ApiProjectAreaWorkPackage[]>;
+    }
 
     static getProjectArea(projectId: string, projectAreaId: number, user: User): Promise<ApiProjectArea> {
         return Http.get(`${Http.baseUrl()}/projects/${projectId}/areas/${projectAreaId}`, user) as unknown as Promise<ApiProjectArea>;
     };
 
-    static createProjectArea(projectId: string, projectArea: ApiProjectArea, user: User): Promise<ApiProjectArea> {
-        return Http.post(`${Http.baseUrl()}/projects/${projectId}/areas`, user, projectArea) as unknown as Promise<ApiProjectArea>;
+    static listProjectAreas(projectId: string, user: User): Promise<ApiProjectArea[]> {
+        return Http.get(`${Http.baseUrl()}/projects/${projectId}/areas`, user) as unknown as Promise<ApiProjectArea[]>;
+    }
+
+    static listProjectAreaWorkPackages(projectId: string, projectAreaId: number, user: User): Promise<ApiProjectAreaWorkPackage[]> {
+        return Http.get(`${Http.baseUrl()}/projects/${projectId}/areas/${projectAreaId}/work-packages`, user) as unknown as Promise<ApiProjectAreaWorkPackage[]>;
+    }
+
+    static listProjectAreaWorkPackagesForProject(projectId: string, user: User): Promise<ApiProjectAreaWorkPackage[]> {
+        return Http.get(`${Http.baseUrl()}/projects/${projectId}/areas/work-packages`, user) as unknown as Promise<ApiProjectAreaWorkPackage[]>;
     }
 };
 
