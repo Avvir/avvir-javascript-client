fa64f449f395a120c517c311cfe5fc08635cc9a0
abhinav-oruganti <abhinav.oruganti@hexagon.com>
2024-09-11 16:43:41 +0530

    few changes to format date fields [BSI-335]

--- source/models/api/api_scandataset_stats.ts
+++ source/models/api/api_scandataset_stats.ts
@@ -16,24 +16,13 @@ export class ApiScanDatasetStats {
    realityCaptureName?: string;
    realityCaptureType?: string;
    realityCaptureUploadedOn?: Date;
-   qaStartedOn?: Date;
-   qaCompletedOn?: Date;
 
    constructor(props: ApiScanDatasetStatsProps) {
-      if (props.qaCompleteTime) {
-         this.qaCompletedOn = new Date(props.qaCompleteTime * 1000);
-      }
-      if (props.qaStartTime) {
-         this.qaStartedOn = new Date(props.qaStartTime * 1000);
-      }
-      if (props.realityCaptureUploadedOn) {
-         this.realityCaptureUploadedOn = new Date(props.realityCaptureUploadedOn * 1000);
-      }
-
       addInstantGetterAndSetterToApiModel(this, 'pipelineStartTime', props.pipelineStartTime);
       addInstantGetterAndSetterToApiModel(this, 'pipelineEndTime', props.pipelineEndTime);
       addInstantGetterAndSetterToApiModel(this, 'qaCompleteTime', props.qaCompleteTime);
       addInstantGetterAndSetterToApiModel(this, 'qaStartTime', props.qaStartTime);
+      addInstantGetterAndSetterToApiModel(this, 'realityCaptureUploadedOn', props.realityCaptureUploadedOn);
       this.organizationName = props.organizationName;
       this.projectName = props.projectName;
       this.scanDatasetId = props.scanDatasetId;
