061e9cade1cce470323291b2700a8840112ce1e9
abhinav-oruganti <abhinav.oruganti@hexagon.com>
2025-11-17 16:32:46 +0530

    add assignee to JiraIssueRequestModel [CAP-949]

--- source/models/api/integrations/jira/FieldsConfiguration.ts
+++ source/models/api/integrations/jira/FieldsConfiguration.ts
@@ -1,9 +1,11 @@
 import {JiraFieldProperty} from "./JiraFieldProperty";
+import {JiraFieldAssignee} from "./JiraFieldAssignee";
 
 export class FieldsConfiguration {
     buildingTypes?: JiraFieldProperty[];
     priorityTypes?: JiraFieldProperty[];
     requestTypes?: JiraFieldProperty[];
+    assignees?: JiraFieldAssignee[];
 }
 
 export default FieldsConfiguration;
---
+++ source/models/api/integrations/jira/JiraFieldAssignee.ts
@@ -0,0 +1,4 @@
+export class JiraFieldAssignee {
+    accountId: string;
+    displayName: string;
+}
