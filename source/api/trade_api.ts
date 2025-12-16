6acff8a04aeffed88450284d9afc48cf7e8fc418
abhinav-oruganti <abhinav.oruganti@hexagon.com>
2025-12-16 09:49:42 +0530

    check status of refresh materialized view [CAP-981]

--- source/api/trade_api.ts
+++ source/api/trade_api.ts
@@ -33,6 +33,12 @@ export default class TradeApi {
     return Http.post(url, user) as unknown as Promise<void>;
   }
 
+  static checkStatusOfLastRefreshCapturedTradeCosts(user: User) {
+    const projects = 'all';
+    const url = `${Http.baseUrl()}/projects/${projects}/trade-breakdown/check-status-update-captured-trade-costs`;
+    return Http.get(url, user) as unknown as Promise<boolean>;
+  }
+
   static listTradeCosts({ projectId }: AssociationIds, user: User) {
     const url = `${Http.baseUrl()}/projects/${projectId}/trade-breakdown/costs`;
     return Http.get(url, user) as unknown as Promise<ApiTradeCost[]>;
