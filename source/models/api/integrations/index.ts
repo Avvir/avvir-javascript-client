a28c37c444e4f6db3dab0f96c53d10181040db9f
abhinav-oruganti <abhinav.oruganti@hexagon.com>
2026-03-07 12:26:35 +0530

    add endpoint to fetch procore coordination issue assignees [CAP-1325]

--- source/models/api/integrations/index.ts
+++ source/models/api/integrations/index.ts
@@ -1,4 +1,7 @@
 export * from "./autodesk";
 export * from "./procore/api_observation_request";
 export * from "./procore/api_rfi_request";
+export * from "./procore/Location";
+export * from "./procore/api_coordination_issue_request";
+export * from "./procore/Trade";
 export * from "./integration_ids";
