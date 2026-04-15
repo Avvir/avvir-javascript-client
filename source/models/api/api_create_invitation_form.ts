64ee5764cd8afd772d739a4cfbd861d594830353
April Groves <april.groves@hexagon.com>
2026-04-15 13:08:28 -0700

    Add userRoles to ApiCreateInvitationForm [HMA-1302]

--- source/models/api/api_create_invitation_form.ts
+++ source/models/api/api_create_invitation_form.ts
@@ -1,10 +1,12 @@
 import UserRole from "../enums/user_role";
+import { ApiUserRole } from "./api_user_role";
 
 export interface ApiCreateInvitationForm {
     userEmail: string
     role: UserRole
     clientAccountId: string
     projectId?: string
+    userRoles?: ApiUserRole[];
 }
 
 export default ApiCreateInvitationForm;
