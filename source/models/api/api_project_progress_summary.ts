cda7136c43308b31387f4741cf7a81e7f4a481ad
abhinav-oruganti <abhinav.oruganti@hexagon.com>
2026-06-26 23:08:37 +0530

    add "last updated" date fields to project summary donut chart models [HMA-1711]

--- source/models/api/api_project_progress_summary.ts
+++ source/models/api/api_project_progress_summary.ts
@@ -1,5 +1,11 @@
 export class ApiProjectProgressSummary {
   built: number;
+  latestBuiltAt: number | null;
+  latestFixedAt: number | null;
+  latestScanDate: number | null;
+  latestAnalysisCompletedAt: number | null;
+  latestDeviationChangedAt: number | null;
+  latestUpdatedAt: number | null;
 }
 
 export default ApiProjectProgressSummary;
